35c8b41b078c99387f3cf1c4d0b9242700116b85
Barbara Perina Bezerra <babiperina@MacBook-Air-de-Barbara.local>
2024-09-20 13:59:08 -0300

    inicio refatoracoes outro layout

--- src/Components/CouponDetail.js
+++ src/Components/CouponDetail.js
@@ -38,28 +38,16 @@ function CouponDetail() {
 
   return (
     <div style={styles.container}>
-      <div style={styles.card}>
-        <div style={styles.header}>
-          <span>@tiacucasobremesas</span>
-          <span style={styles.title}>TIA CUCA SOBREMESAS</span>
+      <div style={styles.upContainer}>
+        <div style={styles.contactInfo}>
+          <h1 style={styles.h1Contact}>@tiacucasobremesas</h1>
+          <h1 style={styles.h1Contact}>(85) 99762-7499</h1>
         </div>
-        
-        <div style={styles.mainContent}>
-          <h2 style={styles.discount}>Você ganhou <strong style={styles.discountPercentage}>{coupon.discount}</strong> de desconto!</h2>
-
-          <div style={styles.rules}>
-            <p style={styles.info}>
-              Este cupom é válido apenas para os pedidos feitos pelo WhatsApp.
-            </p>
-            <p style={styles.info}>
-              Este cupom é válido até <strong>{new Date(coupon.expiration_date).toLocaleDateString()}</strong>.
-            </p>
-            <p style={styles.info}>
-              Este cupom só poderá ser utilizado uma vez.
-            </p>
-          </div>
+        <div style={styles.discountInfo}>
+          <p>Você ganhou</p>
+          <span style={styles.discountSpanStyle}>10%</span>
+          <p>de desconto!</p>
         </div>
-
         <div style={styles.couponSection}>
           <span style={styles.couponLabel}>Código do Cupom</span>
           <div style={styles.couponCodeContainer}>
@@ -67,7 +55,15 @@ function CouponDetail() {
             <button onClick={handleCopy} style={styles.copyButton}>COPIAR</button>
           </div>
         </div>
-
+        <div style={styles.rulesInfo}>
+          <p>
+            Este cupom é válido apenas para os pedidos feitos pelo WhatsApp.<br></br>
+            Este cupom é válido até <strong>{new Date(coupon.expiration_date).toLocaleDateString()}</strong>.<br></br>
+            Este cupom só poderá ser utilizado uma vez.
+          </p>
+        </div>
+      </div>
+      <div style={styles.downContainer}>
         <a href="https://api.whatsapp.com/send?phone=SEU_NUMERO&text=Oi, quero usar o cupom!" style={styles.whatsappButton}>
           FAÇA SEU PEDIDO AGORA
         </a>
@@ -79,56 +75,40 @@ function CouponDetail() {
 const styles = {
   container: {
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     height: '100vh',
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#ff3'
   },
-  card: {
-    backgroundColor: '#FFD447',
-    padding: '20px',
-    borderRadius: '10px',
-    width: '400px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-    textAlign: 'center',
+
+  upContainer: { 
+    width: '70%',
   },
-  header: {
+
+  contactInfo: {
     display: 'flex',
     justifyContent: 'space-between',
-    marginBottom: '15px',
-  },
-  title: {
-    fontWeight: 'bold',
-  },
-  mainContent: {
-    display: 'flex',        // Ativa o layout flex
-    flexDirection: 'row',  
-    gap: '10px',  
-  },
-  rules: {
-    display: 'flex',        // Ativa o layout flex
-    flexDirection: 'column',  
     alignItems: 'center',
-    justifyContent: 'center'
   },
-  discount: {
-    fontSize: '22px',
-    color: '#333',
+  h1Contact: {
+    fontSize: '1.25rem', // Aproximadamente 20px, mas escalável
+  },
+  discountInfo: {
     display: 'flex',
-    flexDirection: 'column',   // Garante que os elementos sejam empilhados em uma coluna
-    alignItems: 'flex-start',
-    minWidth: '140px',  // Define a largura mínima de 140px
-  },
-  discountPercentage:{
-    fontSize: '65px',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
     textAlign: 'center',
+    fontSize: '1.25rem', // Aproximadamente 20px, mas escalável
   },
-  info: {
-    fontSize: '14px',
-    color: '#333',
-    margin: '10px 0',   // Garante que os elementos sejam empilhados em uma coluna
-    textAlign: 'left',  // Alinha os itens à esquerda
-  },  
+  discountSpanStyle: {
+    fontSize: '4.5rem', // Aproximadamente 72px, mas escalável
+    fontWeight: 'bold',
+    lineHeight: '1', // Ajuste de altura da linha para que o número não fique muito espaçado
+  },
+
+    
   couponSection: {
     marginTop: '20px',
   },
@@ -147,7 +127,7 @@ const styles = {
     border: 'none',
     fontSize: '16px',
     width: '75%',
-    textAlign: 'center',
+    textAlign: 'left',
     fontWeight: 'bold',
   },
   copyButton: {
@@ -158,6 +138,12 @@ const styles = {
     cursor: 'pointer',
     fontWeight: 'bold'
   },
+
+
+  downContainer: { 
+    width: '70%',
+  },
+
   whatsappButton: {
     display: 'inline-block',
     marginTop: '20px',
@@ -165,9 +151,10 @@ const styles = {
     backgroundColor: '#38C857',
     color: '#fff',
     textDecoration: 'none',
-    width: '92%',
     borderRadius: '5px',
+    textAlign: 'center',
     fontWeight: 'bold',
+    width: '100%',
   },
 };
 
